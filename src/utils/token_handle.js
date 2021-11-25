bfa4e97892132af48c7ca0f8ec2f05daba18875d
nwokoyepraise <nwokoyepraise@gmail.com>
2021-11-25 10:07:35 +0100

    add utils functions

---
+++ src/utils/token_handle.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const argon2 = require('argon2');
+const mjwt = require('jsonwebtoken');
+const crypt_gen = require('../utils/crypt_gen');
+const key = process.env.JWTKEY;
+
+
+module.exports.gen_rjwt = async function () {
+    try {
+        let rjwt = {};
+        const mrjwt = crypt_gen.gen(12);
+        const _mrjwt = await argon2.hash(mrjwt);
+
+        rjwt.value = mrjwt;
+        rjwt.hash = _mrjwt;
+
+        return rjwt;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports.hash_password = async function (password) {
+    try {
+        return await argon2.hash(password);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports.chk_jwt = async function (user_id, jwt) {
+    try {
+        var res0 = await mjwt.verify(jwt, key);
+        //if (!res0.email_verified) { return { status: false, message: 'Email not verified!' } }
+        if (res0.user_id == user_id) {
+            return { status: true }
+        } else {
+            return { status: false, message: 'JWT and user_id mismatch' }
+        }
+
+    } catch (error) {
+        console.error(error);
+        if (error.name == 'TokenExpiredError') {
+            return { status: false, message: 'TokenExpiredError' }
+        }
+        return { status: false, message: 'Not Allowed' }
+    }
+}
+
+module.exports.gen_jwt = function (user) {
+    return mjwt.sign(user, key, { expiresIn: '86400s' });
+}
+
+module.exports.get_auth = function (authHeader) {
+    let auth;
+    if (!authHeader) { return '' }
+    auth = authHeader.split(' ')[1];
+    return auth;
+}
